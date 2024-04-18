d4bac06517b62d9bf398947de7b847557e8f7ca7
AAyoubelbakhti <ayoubelbak13@gmail.com>
2024-04-19 00:18:22 +0200

    comentarios en el código

--- GestorDeEnlaces/addon/globalPlugins/LinkManager.py
+++ GestorDeEnlaces/addon/globalPlugins/LinkManager.py
@@ -1,3 +1,4 @@
+#Imports
 import globalVars
 import os
 import globalPluginHandler
@@ -7,6 +8,7 @@ import webbrowser
 import json
 import gui
 
+#clase para el gestor de enlaces
 class LinkManager(wx.Dialog):
     def __init__(self, parent, title):
         super(LinkManager, self).__init__(parent, title=title, size=(500, 400))
@@ -14,7 +16,7 @@ class LinkManager(wx.Dialog):
         self.panel = wx.Panel(self)
         self.vbox = wx.BoxSizer(wx.VERTICAL)
         self.panel.SetSizer(self.vbox)
-
+       Lista para mostrar los enlaces
         self.linkList = wx.ListCtrl(self.panel, style=wx.LC_REPORT)
         self.linkList.InsertColumn(0, 'Titulo', width=400)
         self.linkList.SetFocus()
@@ -22,16 +24,18 @@ class LinkManager(wx.Dialog):
         self.loadLinks()
         self.vbox.Add(self.linkList, proportion=1, flag=wx.EXPAND)
 
+        # Panel para añadir enlaces
         self.addLinkPanel = wx.Panel(self.panel)
         addLinkBox = wx.BoxSizer(wx.HORIZONTAL)
 
+        #Campos de texto para el título y la URL
         lblTitle = wx.StaticText(self.addLinkPanel, label="Título:")
         self.txtTitle = wx.TextCtrl(self.addLinkPanel)
         lblUrl = wx.StaticText(self.addLinkPanel, label="URL:")
         self.txtUrl = wx.TextCtrl(self.addLinkPanel)
         self.addBtn = wx.Button(self.addLinkPanel, label='Guardar')
         self.addBtn.Bind(wx.EVT_BUTTON, self.onAddOrEditLink)
-
+        # Añadir los elementos al panel de añadir enlaces
         addLinkBox.Add(lblTitle, flag=wx.RIGHT, border=5)
         addLinkBox.Add(self.txtTitle, proportion=1, flag=wx.EXPAND | wx.RIGHT, border=5)
         addLinkBox.Add(lblUrl, flag=wx.RIGHT, border=5)
@@ -41,7 +45,7 @@ class LinkManager(wx.Dialog):
         self.addLinkPanel.Hide()
 
         self.vbox.Add(self.addLinkPanel, flag=wx.EXPAND | wx.TOP | wx.BOTTOM, border=10)
-
+        # eventos y funciones
         self.linkList.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.openLink)
         self.Bind(wx.EVT_CHAR_HOOK, self.onKeyPress)
 
@@ -50,9 +54,11 @@ class LinkManager(wx.Dialog):
         self.Centre()
         self.Show()
 
+    # Devuelve la ruta del archivo JSON de enlaces
     def getJsonPath(self):
         return os.path.join(globalVars.appArgs.configPath, "links.json")
 
+    # Carga los enlaces desde el archivo JSON
     def loadLinks(self):
         self.linkList.DeleteAllItems()
         self.links.clear()
@@ -69,6 +75,7 @@ class LinkManager(wx.Dialog):
             print("Error al decodificar JSON. Verifique el contenido del archivo.")
             self.saveLinks()
 
+    # Guarda los enlaces en el archivo JSON
     def saveLinks(self):
         with open(self.getJsonPath(), 'w') as file:
             json.dump(self.links, file)
@@ -98,6 +105,7 @@ class LinkManager(wx.Dialog):
             self.addLinkPanel.Hide()
             self.panel.Layout()
 
+    #Borrar los enlaces
     def deleteLink(self):
         index = self.linkList.GetFirstSelected()
         if index != -1:
@@ -107,12 +115,14 @@ class LinkManager(wx.Dialog):
                 self.saveLinks()
                 wx.MessageBox('Enlace borrado', 'Info', wx.OK | wx.ICON_INFORMATION)
 
+    #abrir los links en el navegador
     def openLink(self, event):
         title = self.linkList.GetItemText(event.GetIndex())
         url = self.links.get(title)
         if url:
             webbrowser.open(url)
 
+    # Gestión de los atajos de teclado
     def onKeyPress(self, event):
         keycode = event.GetKeyCode()
         if event.ControlDown() and keycode == ord('A'):
@@ -123,6 +133,7 @@ class LinkManager(wx.Dialog):
             self.deleteLink()
         event.Skip()
 
+    # Muestra u oculta el panel  para añadir enlaces
     def toggleAddLinkPanel(self):
         if self.addLinkPanel.IsShown():
             self.addLinkPanel.Hide()
@@ -131,6 +142,7 @@ class LinkManager(wx.Dialog):
             self.txtTitle.SetFocus()
         self.panel.Layout()
 
+    # Editar enlaces
     def editLink(self):
         index = self.linkList.GetFirstSelected()
         if index != -1:
@@ -144,6 +156,7 @@ class LinkManager(wx.Dialog):
                 self.panel.Layout()
             self.txtTitle.SetFocus()
 
+#Iniciar la interfaz
 def start_link_manager():
     gui.mainFrame.prePopup()
     frame = LinkManager(gui.mainFrame,'Gestor de Enlaces')
@@ -151,5 +164,6 @@ def start_link_manager():
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
     @script(description='Abre la ventana del gestor de enlaces', gesture='kb:NVDA+alt+k', category='Gestor De Enlaces')
+    #Abre La interfaz mediante un gesto
     def script_open_file(self, gesture):
         wx.CallAfter(start_link_manager)
