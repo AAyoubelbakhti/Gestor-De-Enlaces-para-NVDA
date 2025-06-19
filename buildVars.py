50d6e937369f647f8cbe1d0143da34f340e4f6f8
Ángel Alcántar <rayoalcantar@gmail.com>
2025-06-19 13:35:08 -0600

    Se actualizan las versiones testeadas de NVDA y la versión del addon

--- buildVars.py
+++ buildVars.py
@@ -27,7 +27,7 @@ addon_info = {
 Pequeño complemento para gestionar y guardar links de forma rápida
 """),
 	# version
-	"addon_version": "1.3.0",
+	"addon_version": "1.3.6",
 	# Author(s)
 	"addon_author": "Ayoub <ayoubelbak13@gmail.com>",
 	# URL for the add-on documentation support
@@ -39,7 +39,7 @@ Pequeño complemento para gestionar y guardar links de forma rápida
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": "2024.1",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2025.1.0",
+	"addon_lastTestedNVDAVersion": "2025.1.1",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
